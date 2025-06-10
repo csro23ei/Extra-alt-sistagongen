b878943bcfb16a0860daef45457fbf7fd7afbd9d
csro23ei <dante030724@gmail.com>
2025-06-10 11:14:46 +0200

    oke

--- Extra-alt-sistagongen/src/main/java/Extra_alt_sistagongen/Extra_alt_sistagongen/CorsConfig.java
+++ Extra-alt-sistagongen/src/main/java/Extra_alt_sistagongen/Extra_alt_sistagongen/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Next.js frontend
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5173") // Next.js frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
